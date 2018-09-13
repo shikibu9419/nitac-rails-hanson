bbd08476e3000d3603f607bacf9e8526ae116960
Shikibu <shikibu9419@gmail.com>
2018-09-13 22:21:22 +0900

    Add devise User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'todos#index'
+  devise_for :users
   resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
